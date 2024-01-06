c746701d584c178eb81295b9e1f0f0e30df07cec
ShashiKumarRVU <155731415+ShashiKumarRVU@users.noreply.github.com>
2024-01-06 10:05:09 +0530

    porto

---
+++ script.js
@@ -0,0 +1,85 @@
+
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
+
+
+
+  
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+
+  new PureCounter();
+
+})()
+function displayGreeting() {
+  var userName = document.getElementById("name").value;
+  var greetingMessage = "Hello, " + userName + "! \nWelcome to the Shashi Resume Web page!<br>To visit the website click <a href='file:///C:/Users/shash/Downloads/jury_project/shashi_resume/shashi.html'style='color:yellow'>here</a>";
+
+  var greetingElement = document.getElementById("greetingMessage");
+  greetingElement.innerHTML = greetingMessage;
+
+  // Add a class to trigger the transition
+  greetingElement.classList.add("fadeIn");
+}
